e364bec5fd4e6b2442d42b85e661f03b5e79f3ea
Nika <skozirev1986@mail.ru>
2022-12-06 23:01:58 +0300

    Готово

---
+++ Task 4/Program.cs
@@ -0,0 +1,25 @@
+﻿
+Console.WriteLine("Введите первое число ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число ");
+
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число ");
+
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a>b && a>c)
+{
+    Console.WriteLine($"Max = {a}");
+}
+else if (b>a && b>c)
+{
+    Console.WriteLine($"Max = {b}");
+}
+else if (c>a && c>b)
+{
+    Console.WriteLine($"Max = {c}");
+}
---
+++ Task 8/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.Write("Введите число ");
+
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+while (count<= N)
+{
+    if(count%2==0)
+    {
+        Console.Write($"{count} ");
+        count++;
+    }
+    else
+        count++;
+}
